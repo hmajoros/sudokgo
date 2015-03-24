1c19616450cfc8a42bab7e706a00484d8056c88d
Laura Knutilla <laura@areyouahuman.com>
2015-03-24 12:00:30 -0400

    improving error checking

--- static/board.js
+++ static/board.js
@@ -3,12 +3,14 @@
 /*Timer function begins time when "New Game" is pressed, and ends when "Done!" is pressed
 and the board is valid. Based on code at http://jsfiddle.net/AbrGL/8/*/
 //START TIMER SECTION
+        var is_board = false;
         var clicked = false;
         var sec,min;
         
         function startClock() {  
             sec = 0;
-            min = 0;         
+            min = 0;
+            is_board = true;    
             document.getElementById("pause_play_btn").disabled = false;
             if (clicked === false) {
                 clock = setInterval("stopWatch()", 1000);
@@ -46,30 +48,63 @@ and the board is valid. Based on code at http://jsfiddle.net/AbrGL/8/*/
             if (clicked === true) //clock is running->pause
             {
                 stopClock();
-                pause_play.className = "col-md-1 glyphicon glyphicon-play";
+                pause_play.className = "glyphicon glyphicon-play";
             }
-            else
+            else if (is_board === true)
             {
                 restartClock();
-                pause_play.className = "col-md-1 glyphicon glyphicon-pause";
+                pause_play.className = "glyphicon glyphicon-pause";
             }
         }
 
         $("#pause_play_btn").click(function(){
-            $("#sudokuTable").toggle();
+            if (is_board == true) $("#sudokuTable").toggle();
         });
 
         $("#new").click(function(){
-            $("#sudokuTable").show();
+            if (is_board == true) $("#sudokuTable").show();
         });
 
                       
 
 //END TIMER SECTION
+//BEGIN CREATE NUMPAD//
+        var current_cell;//current selected cell
+        function createNumPad() {
+            var num_pad=document.getElementById('numPad');
+            var num_pad_bdy=document.createElement('tbody');
+            var count = 1;
+            for(var i = 0; i < 3; ++i){
+                var row=document.createElement('tr');
+                for(var j = 0; j < 3; ++j){
+                    var td=document.createElement('td');
+                    var number = document.createElement("button");
+                    number.type = "button";
+                    number.className += "btn btn-primary";
+                    number.innerHTML = count;
+                    number.value = count;
+                    number.onclick = function(event) { handleNumPad(this); };
+
+                    td.appendChild(number);
+                    row.appendChild(td);
+                    ++count;
+                }
+                num_pad_bdy.appendChild(row);
+            }
+            num_pad.appendChild(num_pad_bdy);
+        };
+        function handleNumPad(number) {
+            var selected = document.getElementById(current_cell.id);
+            selected.value = number.value;
+            handleBlur(selected);
+        };
+//END CREATE NUMPAD//
 /*createSudoku() creates a base board framework and is executed on page load*/
 //BEGIN CREATE BOARD
+        var solved_board = [];
+        var conflict_list = []; //list of currently conflicting/red board indexes
+        var board_size = 81;
         function createSudoku() {
-            var body=document.getElementsByTagName('body')[0];
             var board=document.getElementById('sudokuTable');
             var boardbdy=document.createElement('tbody');
 
@@ -130,6 +165,10 @@ It is executed on "New Game" press, and calls hideCells(). The algorithm is base
 David J. Rager at http://blog.fourthwoods.com/2011/02/05/sudoku-in-javascript/     
 */
         function fillBoard() {
+            solved_board = [];
+            conflict_list = [];
+            board_size = 81;
+
             var board = document.getElementsByClassName("cell");
             var randomNine = fishYatesShuffle(9);
             //create base sudoku board
@@ -190,7 +229,24 @@ David J. Rager at http://blog.fourthwoods.com/2011/02/05/sudoku-in-javascript/
                     else if (board[j].value === numb2) board[j].value = numb1;
                 }
             }
+            for (var i = 0; i < board.length; ++i)
+            {
+                solved_board[i] = board[i].value;
+            }
+
             hideCells();
+
+         };
+         function solveBoard() {
+            //create base sudoku board
+            var board = document.getElementsByClassName("cell");
+            for (var i = 0; i < solved_board.length; i++) {
+                board[i].value = solved_board[i];
+                board[i].readOnly = true;
+                board[i].style.color = "black";
+            }
+            board_size = 81;
+            checkBoard();
 
          };
 /*hideCells() naiivly hides 4 squares/block. This will create an easy sudoku puzzle that will normally be unique.*/
@@ -208,8 +264,12 @@ David J. Rager at http://blog.fourthwoods.com/2011/02/05/sudoku-in-javascript/
                     var col = cell - row*3;
                     cell = row*9 + block_row*27 + col + block_col*3;
                     board[cell].readOnly = false;
-                    board[cell].onblur = function(event) { checkValid(this); };
+                    board[cell].style.color = "blue";
+                    //board[cell].addEventListener("click", handleNumPad());
+                    board[cell].onclick = function(event) { handleClick(this); };
+                    board[cell].onblur = function(event) { handleBlur(this); };
                     board[cell].value = "";
+                    --board_size;
                 }
             }
         } 
@@ -233,13 +293,59 @@ Returns the scrambled array. */
 //BEGIN VALIDATION SECTION
 /*checkValid(numb) is called everytime a user clicks off of a filled cell entry. Checks to see if entry
 matches other cells in the same block, column, or section. Returns bool: true if valid, false if not*/
-        function checkValid(numb) {
-            var valid = true;
-            if(numb.value != "") {
-                if (numb.value % 1 != 0 || numb.value > 9 || numb.value < 1) {
-                    alert("Invalid Entry: Please enter a number between 1 - 9. You entered " + numb.value);
+        var old_val;//val of currently selected
+        var new_val;//val of what the user changes the current value to
+        function handleClick(numb) {
+            current_cell = document.getElementById(numb.id);
+            old_val = document.getElementById(numb.id).value;
+        }
+        function handleBlur(numb) {
+            new_val = document.getElementById(numb.id).value;
+            console.log("old " + old_val + "new" + new_val);
+            if (new_val != old_val) //did the user change the value
+            {
+                removeCorrectedConflicts();
+                if (checkValid(numb) && (numb.style.color === "red"))//if the user fixes a cell
+                {
+                    document.getElementById(numb.id).style.color = "blue";
                 }
             }
+            //need to handle if a user deletes a previously valid number (they know it's wrong before AI)  
+        }
+        //checks if cell is valid and turns it red if it is invalid
+        function checkValid(numb) {
+            var valid = true;        
+            if(numb.value != "") {
+                if (numb.value % 1 != 0 || numb.value > 9 || numb.value < 1) {
+                    document.getElementById(numb.id).style.color = "red";
+                    alert("Invalid Entry: Please enter a number between 1 - 9. You entered " + numb.value);
+                    return false;
+                }
+            }
+            else return true; //a blank space is valid
+            var board = document.getElementsByClassName("cell");           
+            if (checkConflict(numb, false)) {
+                valid = false;
+                document.getElementById(numb.id).style.color = "red";
+                for (var i = 0; i < conflict_list.length; ++i)
+                {
+                    var j = conflict_list[i];
+                    var comp = document.getElementById(board[j].id);
+                    if (comp.readOnly === true) comp.style.color = "#990000";
+                    else comp.style.color = "red";
+                }
+            }
+
+            if (valid) {
+                ++board_size;
+                console.log(board_size);
+            }
+            checkBoard();
+            return valid;
+        };
+        //finds conflicts with the number
+        function checkConflict(numb, board_check) { 
+            var conflict = false;
             var board = document.getElementsByClassName("cell");
             var check = document.getElementById(numb.id);
             check = check.id.split("");
@@ -252,37 +358,40 @@ matches other cells in the same block, column, or section. Returns bool: true if
                 if (row === check[0] && col === check[1]) continue;
                 if (row === check[0] || col === check[1] || block === check[2]) {
                     if (numb.value === board[i].value && numb.value != "") {
-                        valid = false;
-                        document.getElementById(numb.id).style.color = "red";
-                        document.getElementById(board[i].id).style.color = "red";
-                    }
-                    else {
-                        document.getElementById(board[i].id).style.color = "black";
-                        if (valid) document.getElementById(numb.id).style.color = "black";
+                        conflict = true;
+                        //will add to conflict list unless reviewing conflict list
+                        if (!board_check) conflict_list[conflict_list.length] = i;
+
                     }
                 }
             }
-            return valid;
+            return conflict;
+        };
+
+        function removeCorrectedConflicts() {
+            if (conflict_list.length > 0)
+            {
+                var board = document.getElementsByClassName("cell");           
+                for (var i = conflict_list.length-1; i >= 0; --i)
+                {
+                    var j = conflict_list[i];
+                    var comp = document.getElementById(board[j].id);
+                    if (!checkConflict(comp, true)) {
+                        conflict_list.splice(i,1);
+                        if (comp.readOnly === true) comp.style.color = "black";
+                        else comp.style.color = "blue";
+                    }
+                    else console.log("still conflict");
+                }
+            }
         };
 //checkBoard() checks the entire board to see if valid and stops the timer if it the valid
         function checkBoard() {
-            var board = document.getElementsByClassName("cell");
-            var valid = true;
-            for (var i = 0; i < board.length; ++i) {
-                if (board[i].value === "" || board[i].value === undefined) {
-                    valid = false;
-                }
-                if (!checkValid(board[i])) {
-                    valid = false;
-                }
-            }
-            if (valid) {
-               stopClock(); 
+            if (board_size >= 81) {
+                console.log("CONGRATS");
                alert("CONGRATS! " + "You finished in: " + convertToTime(min) + ":" + convertToTime(sec));
+               stopClock(); 
             } 
-            else {
-                alert("Not done yet!");
-            }
         }
 //END VALIDATION SECTION
 
