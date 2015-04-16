c822dd57226fac967bf9386b0dc1dc78e2677ce8
Laura Knutilla <laura@areyouahuman.com>
2015-04-16 19:51:43 -0400

    added spinner to invite functionality

--- static/client_2p.js
+++ static/client_2p.js
@@ -7,8 +7,27 @@
 
         $(document).on('keydown', updateBoard);
 
-        //socket.emit('create_room');
-        
+        var opts = {
+          lines: 11, // The number of lines to draw
+          length: 32, // The length of each line
+          width: 7, // The line thickness
+          radius: 60, // The radius of the inner circle
+          corners: 1, // Corner roundness (0..1)
+          rotate: 19, // The rotation offset
+          direction: 1, // 1: clockwise, -1: counterclockwise
+          color: '#000', // #rgb or #rrggbb or array of colors
+          speed: 1.2, // Rounds per second
+          trail: 64, // Afterglow percentage
+          shadow: false, // Whether to render a shadow
+          hwaccel: false, // Whether to use hardware acceleration
+          className: 'spinner', // The CSS class to assign to the spinner
+          zIndex: 2e9, // The z-index (defaults to 2000000000)
+          top: '50%', // Top position relative to parent
+          left: '50%' // Left position relative to parent
+        };
+        var spinner_div = document.getElementById('spinner');
+        var spinner = new Spinner().spin();
+        spinner_div.appendChild(spinner.el);
         // createSudoku();
         // createNumPad();
     });
@@ -43,24 +62,24 @@
         console.log("clicked");
         $(this).attr("disabled", "disabled");
         socket.emit('create_room');
+        $("#spinner").removeClass("hide");
     });
 
     $('#start').click(function() {
         $(this).attr("disabled", "disabled");
         socket.emit('user_ready');
+        $("#spinner").removeClass("hide");
     })
 
     socket.on('room_created', function(room) {
         //$('form').addClass('hide');
-        console.log("modalling");
-        $("#inviteModal .modal-body").html('Invite your friends by sending them this link: ' + document.URL + '?id=' + room);
+        $("#inviteModal .modal-body").html('Invite your friends by sending them this link: <div class="alert alert-info">' + document.URL + '?id=' + room + '</div>');
         $("#inviteModal").modal("show");
         $('.container:first').append('<div class="alert alert-danger">Now joined room ' + room + '. Invite your friends by sending them this link: ' + document.URL + '?id=' + room + '</div>');
     });
 
     socket.on('game_ready', function() {
-        //spinner start
-        //$('#join').addClass('hide');
+        $("#spinner").addClass("hide");
         $('#invite').addClass('hide');
         $('#start').removeClass('hide'); //start game button appear
         $('.alert').remove();
@@ -68,6 +87,7 @@
 
     socket.on('start_game', function(board) {
         //spinner stop
+        $("#spinner").addClass("hide");
         $('#start').addClass('hide');
         for (var i = 0; i < board.length; i++) {
             var cell = $('.cell')[i];
